38d548bd8355844d385dc575874ae28eea0549cf
최현정 <hjchoi.dev@gmail.com>
2025-08-01 17:41:12 +0900

    98. Validate Binary Search Tree

---
+++ src/main/kotlin/Q98.kt
@@ -0,0 +1,56 @@
+/**
+ * 98. Validate Binary Search Tree
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ */
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+private fun isValidBST(root: TreeNode?): Boolean {
+    val values = mutableListOf<Int>()
+    inorderTraversal(root, values)
+
+    for (i in 1 until values.size) {
+        if (values[i] <= values[i - 1]) return false
+    }
+
+    return true
+}
+
+private fun inorderTraversal(root: TreeNode?, values: MutableList<Int>) {
+    if (root == null) return
+
+    inorderTraversal(root.left, values)
+    values.add(root.`val`)
+    inorderTraversal(root.right, values)
+}
+
+fun main() {
+    val validBST = TreeNode(5).apply {
+        left = TreeNode(3).apply {
+            left = TreeNode(2)
+            right = TreeNode(4)
+        }
+        right = TreeNode(8).apply {
+            left = TreeNode(7)
+            right = TreeNode(9)
+        }
+    }
+
+    println(isValidBST(validBST))
+
+    val invalidBST = TreeNode(5).apply {
+        left = TreeNode(3).apply {
+            left = TreeNode(2)
+            right = TreeNode(6) // 이게 문제!
+        }
+        right = TreeNode(8).apply {
+            left = TreeNode(7)
+            right = TreeNode(9)
+        }
+    }
+
+    println(isValidBST(invalidBST))
+}
