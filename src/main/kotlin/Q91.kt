88e5d4d9e9b41fa0935cc745b5d6327e43fc959c
최현정 <hjchoi.dev@gmail.com>
2025-08-06 19:33:35 +0900

    91. Decode Ways

---
+++ src/main/kotlin/Q91.kt
@@ -0,0 +1,36 @@
+/**
+ * 91. Decode Ways
+ * https://leetcode.com/problems/decode-ways
+ */
+
+private fun numDecodings(s: String): Int {
+    if (s.isEmpty() || s[0] == '0') return 0
+
+    val n = s.length
+    val dp = IntArray(n + 1)
+
+    dp[0] = 1
+    dp[1] = 1
+
+    for (i in 2..n) {
+        val currentChar = s[i - 1]
+        val prevChar = s[i - 2]
+
+        if (currentChar != '0') {
+            dp[i] += dp[i - 1]
+        }
+
+        val twoDigit = (prevChar - '0') * 10 + (currentChar - '0')
+        if (twoDigit in 10..26) {
+            dp[i] += dp[i - 2]
+        }
+    }
+
+    return dp[n]
+}
+
+fun main() {
+    println(numDecodings("12"))     // 2
+    println(numDecodings("226"))    // 3
+    println(numDecodings("06"))     // 0
+}
