96359653111eda09ec84cb8e6766be57a8749a17
최현정 <hjchoi.dev@gmail.com>
2025-08-08 19:40:51 +0900

    Update Q206

--- src/main/kotlin/Q206.kt
+++ src/main/kotlin/Q206.kt
@@ -3,22 +3,16 @@
  * https://leetcode.com/problems/reverse-linked-list
  */
 
-private class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-}
+private fun reverseList(head: ListNode?): ListNode? {
+    var prev: ListNode? = null
+    var current = head
 
-private class Solution {
-    fun reverseList(head: ListNode?): ListNode? {
-        var prev: ListNode? = null
-        var current = head
-
-        while (current != null) {
-            val next = current.next
-            current.next = prev
-            prev = current
-            current = next
-        }
-
-        return prev
+    while (current != null) {
+        val next = current.next
+        current.next = prev
+        prev = current
+        current = next
     }
+
+    return prev
 }
